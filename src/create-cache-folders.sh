b54d34700b4ead38b0d07a5562b19cffbb54aad3
Ethan Gaebel <egaebel@vt.edu>
2017-06-08 22:18:45 -0700

    Added script to create cache folders

---
+++ src/create-cache-folders.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir definition-cache
+mkdir dissimilarity-vector-plots
+mkdir failed-definition-lookups
+mkdir sentence-graphs-storage
+mkdir sentence-graphs-visualization
+mkdir wiktionary-locks
