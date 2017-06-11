2260981988eaeb96f745a777a5070c484fbe97c9
Ethan Gaebel <egaebel@vt.edu>
2017-06-11 01:23:49 -0700

    Added failed parse folder

--- src/create-cache-folders.sh
+++ src/create-cache-folders.sh
@@ -3,6 +3,7 @@
 mkdir definition-cache
 mkdir dissimilarity-vector-plots
 mkdir failed-definition-lookups
+mkdir failed-part-of-speech-parse
 mkdir sentence-graphs-storage
 mkdir sentence-graphs-visualization
 mkdir wiktionary-locks
